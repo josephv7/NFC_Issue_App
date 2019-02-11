36488671facf66fa1344dc9dedc98793beed72ea
josephmathew900 <josephmathew900@gmail.com>
2019-02-11 23:48:27 +0530

    addpatient log added

---
+++ app/src/main/java/iamjosephvarghese/rm/com/nfcissueapp/AddPatientLogActivity.java
@@ -0,0 +1,13 @@
+package iamjosephvarghese.rm.com.nfcissueapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPatientLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_patient_log);
+    }
+}
