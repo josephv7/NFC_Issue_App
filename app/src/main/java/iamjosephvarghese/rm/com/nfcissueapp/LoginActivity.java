bf90580f6790f540126a5183cad3ea1d6bbc4316
josephmathew900 <josephmathew900@gmail.com>
2019-02-11 14:53:43 +0530

    Added Login Activity

---
+++ app/src/main/java/iamjosephvarghese/rm/com/nfcissueapp/LoginActivity.java
@@ -0,0 +1,13 @@
+package iamjosephvarghese.rm.com.nfcissueapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
